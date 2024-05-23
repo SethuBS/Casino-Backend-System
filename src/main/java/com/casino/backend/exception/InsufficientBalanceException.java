bd95cdf2bf942414657e10d3e654718bb4cb1b7c
Sethu <sethuserge@gmail.com>
2024-05-23 13:50:39 +0200

    Added exception classes

---
+++ src/main/java/com/casino/backend/exception/InsufficientBalanceException.java
@@ -0,0 +1,11 @@
+package com.casino.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
